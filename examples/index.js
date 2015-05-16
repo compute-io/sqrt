387f9aa40ad135d67894df6ea8c7bca0c4d190f5
kgryte <kgryte@gmail.com>
2015-05-16 12:06:39 -0700

    [UPDATE] dotfiles. Makefile. validation deps. dev deps. add accessor and copy opts.

--- examples/index.js
+++ examples/index.js
@@ -2,13 +2,10 @@
 
 var sqrt = require( './../lib' );
 
-// Simulate some data...
 var data = new Array( 100 );
-
 for ( var i = 0; i < data.length; i++ ) {
 	data[ i ] = Math.round( Math.random()*1000 );
 }
+var out = sqrt( data );
 
-sqrt( data );
-
-console.log( data.join( '\n' ) );
+console.log( out.join( '\n' ) );
